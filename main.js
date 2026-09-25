856547020c19312f8950da805963b019b03876d8
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-09-25 09:17:28 -0600

    chore: elimina archivos innecesarios

--- main.js
+++
@@ -1,9 +0,0 @@
-let numeroUsuario = parseInt(prompt("Dame un número: "));
-
-function calcularDoble(numero) {
-    let resultado = numero *2;
-    return resultado;    
-}
-
-let valorCalculado = calcularDoble(numeroUsuario);
-console.log(valorCalculado);
